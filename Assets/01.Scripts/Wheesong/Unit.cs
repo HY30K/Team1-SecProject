2cd8eb5e7b604abcd270f6a81a48b04ae3180ca6
JangWheesung <gameplay1qaz@gmail.com>
2023-09-07 12:12:25 +0900

    UnitParent

--- Assets/01.Scripts/Wheesong/Unit.cs
+++ Assets/01.Scripts/Wheesong/Unit.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public enum State { IDLE = 0, CHASE = 1, ATTACK = 2, DIE = 3, NULL = 4}
 
-public class Unit : MonoBehaviour
+public abstract class Unit : MonoBehaviour
 {
+    public State state;
+
     [Header("SOName")]
     [SerializeField] private string unitSOName;
 
@@ -16,44 +18,87 @@ public class Unit : MonoBehaviour
     protected float range;
     protected float attackRange;
 
+    protected Transform enemyTrs;
+
     private void OnEnable() // »ý¼º ½Ã
     {
         AgentData enemyData = Resources.Load<AgentData>($"EnemySO/{unitSOName}");
         enemyData = new AgentData(out hp, out attack, out speed, out attackDelay, out range, out attackRange);
 
-        IdleNode();
+        state = State.IDLE;
+    }
+
+    private void Update()
+    {
+        UnitBrain();
     }
 
     private void UnitBrain()
     {
-        //switch()
+        switch (state)
+        {
+            case State.IDLE:
+                IdleNode();
+                break;
+            case State.CHASE:
+                ChaseNode();
+                break;
+            case State.ATTACK:
+                AttackNode();
+                break;
+        }
     }
 
-    protected virtual void IdleNode()
+    protected void IdleNode()
     {
+        Idle();
+
         if (DetectionRange(range))
-            ChaseNode();
+            state = State.CHASE;
     }
 
-    protected virtual void ChaseNode()
+    protected void ChaseNode()
     {
-        if (DetectionRange(attackRange))
-            ChaseNode();
-        else if (!DetectionRange(range))
-            IdleNode();
+        Chase();
+
+        if (!DetectionRange(range))
+        {
+            state = State.IDLE;
+        }
+        else if (DetectionLength(attackRange))
+            state = State.ATTACK;
     }
 
-    protected virtual void AttackNode()
+    protected void AttackNode()
     {
-        if(!DetectionRange(attackRange))
-            AttackNode();
+        Attack();
+
+        if (!DetectionLength(attackRange))
+            state = State.CHASE;
     }
 
-    protected bool DetectionRange(float range)
+    protected bool DetectionRange(float range) //idle <=> chase
     {
-        bool getRange = Physics2D.OverlapCircle(transform.position, range, LayerMask.GetMask("Enemy"));
-        return getRange;
+        Collider2D[] getRange = Physics2D.OverlapCircleAll(transform.position, range, LayerMask.GetMask("Enemy"));
+        if (getRange.Length > 0)
+        {
+            enemyTrs = getRange[0].transform;
+
+            return true;
+        }
+        enemyTrs = null;
+        return false;
     }
+
+    protected bool DetectionLength(float range) //chase <=> attack
+    {
+        float length = Vector2.Distance(enemyTrs.position, transform.position);
+        return length <= range ? true : false; 
+    }
+
+    protected abstract void Idle();
+    protected abstract void Chase();
+    protected abstract void Attack();
 
     protected virtual void Die()
     {
