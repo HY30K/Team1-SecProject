e591adf91782d865726e2105168834e8ed3d6f97
JangWheesung <gameplay1qaz@gmail.com>
2023-09-28 14:36:34 +0900

    유닛 시점 변경 시 프로필

--- Assets/01.Scripts/Wheesong/CameraMove.cs
+++ Assets/01.Scripts/Wheesong/CameraMove.cs
@@ -1,8 +1,7 @@
-using Cinemachine;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
+using TMPro;
 
 public class CameraMove : MonoBehaviour
 {
@@ -11,8 +10,18 @@ public class CameraMove : MonoBehaviour
     [SerializeField] private float maxSize;
     [SerializeField] private float minSize;
 
+    [Header("FollowValue")]
+    [SerializeField] private Transform units;
+    [SerializeField] private Transform player;
+
+    [Header("UnitProfile")]
+    [SerializeField] private GameObject unitProfile;
+    [SerializeField] private Image unitImage;
+    [SerializeField] private TextMeshProUGUI unitText;
+
     private CinemachineVirtualCamera vcam;
     private Rigidbody2D rb;
+    private int unitIndex;
 
     private void Awake()
     {
@@ -23,6 +32,36 @@ public class CameraMove : MonoBehaviour
     private void Update()
     {
         MouseWheel();
+        OnFollowCam();
+    }
+
+    private void OnFollowCam()
+    {
+        if (Input.GetMouseButtonDown(1) && units.childCount > 0)
+        {
+            unitProfile.SetActive(true);
+
+            Transform unit = units.GetChild(unitIndex);
+            AgentData unitData = Resources.Load<AgentData>($"UnitSO/{unit.name}");
+            vcam.Follow = unit;
+            unitImage.sprite = unitData.Sprite;
+            unitText.text = $"LV.{unitData.level}";
+
+            if (unitIndex >= units.childCount - 1)
+            {
+                unitIndex = 0;
+            }
+            else
+            {
+                unitIndex++;
+            }
+        }
+        else if(Input.GetAxisRaw("Horizontal") != 0 ||
+            Input.GetAxisRaw("Vertical") != 0)
+        {
+            unitProfile.SetActive(false);
+            vcam.Follow = player;
+        }
     }
 
     private void MouseWheel()
