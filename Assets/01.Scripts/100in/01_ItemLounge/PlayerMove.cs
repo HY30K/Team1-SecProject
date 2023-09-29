2929ad529ddcd3472c9b92e03132057dd627e5c3
dlstjd0237 <a49918074@gmail.com>
2023-09-29 10:20:55 +0900

    줌인 줌아웃

--- Assets/01.Scripts/100in/01_ItemLounge/PlayerMove.cs
+++ Assets/01.Scripts/100in/01_ItemLounge/PlayerMove.cs
@@ -4,27 +4,24 @@ using UnityEngine;
 using DG.Tweening;
 public class PlayerMove : MonoBehaviour
 {
-    Rigidbody2D _rig2D;
-    [SerializeField] private WeaponStatusSO qwer;
     private Camera _cam;
     Vector3 _dir = Vector3.zero;
     private void Awake()
     {
         _cam = Camera.main;
-        _rig2D = GetComponent<Rigidbody2D>();
     }
-    private void FixedUpdate()
+    private void Update()
+    {
+        Moveing();
+    }
+ 
+    private void Moveing()
     {
         if (Input.GetMouseButtonDown(1))
         {
             _dir = _cam.ScreenToWorldPoint(Input.mousePosition);
             _dir.z = 0;
-            transform.DOMove(_dir, 3);
+            transform.DOMove(_dir, 1);
         }
-        
-        //float x = Input.GetAxisRaw("Horizontal");
-        //float y = Input.GetAxisRaw("Vertical");
-        //_rig2D.velocity = new Vector2(x,y)*3;
-
     }
 }
